00db6c83ceb0dcea30332df6373fc617aa42ab53
Camila <camilafolchi@gmail.com>
2019-06-19 19:38:02 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(document).ready(function(){
+  $(".nav-link").click(function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function() {
+        window.location.hash = gato;
+      });
+    }
+  });
+
+});
